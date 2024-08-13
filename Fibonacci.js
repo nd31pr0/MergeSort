c532e09e18616e90e9d317544fb6970163328d43
Ndzi Nfor <ndzinfor@gmail.com>
2024-08-13 21:33:00 +0100

    Recursive implementation of fibonacci

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,3 +1,5 @@
+// Method 1 of fibonacci solution (iteration method)
+
 function fibs(n){
     let fibArr = [];
     for(let i = 0; i < n; i++){
@@ -16,5 +18,21 @@ fibs(8);
 
 
 
-
-
+function fibsRec(n) {
+    // Base case: return an empty array for n <= 0
+    if (n <= 0) return [];
+    // Base case: return [0] for n = 1
+    if (n === 1) return [0];
+    // Base case: return [0, 1] for n = 2
+    if (n === 2) return [0, 1];
+  
+    // Recursive case: get the Fibonacci sequence for n - 1
+    const fib = fibsRec(n - 1);
+    // Add the next Fibonacci number
+    const nextFib = fib[fib.length - 1] + fib[fib.length - 2];
+    fib.push(nextFib);
+  
+    return fib;
+  }
+  
+  console.log(fibsRec(8))
