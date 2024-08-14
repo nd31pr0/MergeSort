d63ce9b3994ecf826c6bfe0ddde79c6bc478bfb1
Ndzi Nfor <ndzinfor@gmail.com>
2024-08-14 22:26:44 +0100

    testing out the algo

--- MergeSort.js
+++ MergeSort.js
@@ -31,3 +31,8 @@ function merge(left, right) {
     // If there are remaining elements in left or right, add them
     return sortedArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
+
+
+// Testing out the algorithm
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));  // Output: [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(mergeSort([105, 79, 100, 110]));          // Output: [79, 100, 105, 110]
